bcd44f5956ef73d9d449f84a0b00ff8155255a07
Adrian030304 <adrianromanenco1@gmail.com>
2026-03-01 01:22:19 +0200

    Implement strucure of a graphical interface

--- InterfataGrafica/src/main/java/org/InterfataGrafica/JavaFxApplication.java
+++ InterfataGrafica/src/main/java/org/InterfataGrafica/JavaFxApplication.java
@@ -1,12 +1,23 @@
 package org.InterfataGrafica;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class JavaFxApplication extends Application {
 
     @Override
     public void start(Stage window) {
+        Button button = new Button("Button for a message");
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+
+        Scene scene = new Scene(componentGroup);
+        window.setScene(scene);
+
         window.setTitle("Bun venit");
         window.show();
     }
