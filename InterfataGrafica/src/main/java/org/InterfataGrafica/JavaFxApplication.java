d96e6291a13bf64b487e33890f6d138db5a3317c
Adrian030304 <adrianromanenco1@gmail.com>
2026-03-01 02:14:16 +0200

    Add visual componenets on a layout so I can set where they stay on screen

--- InterfataGrafica/src/main/java/org/InterfataGrafica/JavaFxApplication.java
+++ InterfataGrafica/src/main/java/org/InterfataGrafica/JavaFxApplication.java
@@ -1,25 +1,34 @@
 package org.InterfataGrafica;
 
-import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.application.Application;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.BorderPane;
 
 public class JavaFxApplication extends Application {
 
     @Override
     public void start(Stage window) {
 
-        Label textComponent = new Label("Textul acesta este folosit pentru label");
+        Label textComponent = new Label("Textul acesta este folosit pentru label ");
         Button button = new Button("Button for a message");
+
+        BorderPane layout = new BorderPane();
+        layout.setTop(new Label("NORD"));
+        layout.setBottom(new Label("SOUTH"));
+        layout.setLeft(new Label("WEST"));
+        layout.setRight(new Label("EAST"));
 
         FlowPane componentGroup = new FlowPane();
         componentGroup.getChildren().add(textComponent);
         componentGroup.getChildren().add(button);
 
-        Scene scene = new Scene(componentGroup);
+//        layout.setCenter(componentGroup);
+        
+        Scene scene = new Scene(layout);
         window.setScene(scene);
 
         window.setTitle("Bun venit");
